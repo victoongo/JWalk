d1740497abc9ad4c6b64c23136889259b875e4a7
Victor Wang <victoongo@gmail.com>
2015-06-17 00:42:50 -0400

    Hellow world in java

---
+++ Udacity_intro/lesson1/hello/HelloPrinter.java
@@ -0,0 +1,11 @@
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("1 + 3");
+        System.out.print(1 + 2);
+        System.out.print(3 * 5 / 7);
+        System.out.println(3*4);
+    }
+}
